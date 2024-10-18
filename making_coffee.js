5935a546c65fd0b376f89ccae24729772197ddc8
AbaevMariya <41700081+AbaevMariya@users.noreply.github.com>
2024-10-18 05:58:28 +0300

    Add files via upload
    recipe making coffee

---
+++ making_coffee.js
@@ -0,0 +1,16 @@
+let coffee, water, cup, sugar, milk
+
+coffee = 'black coffee'
+water = 'cold water'
+sugar = 'grey sugar'
+milk = 'oat milk'
+cup = 'medium cup'
+
+console.log(`1. Pour drinking ${water} into electric kettle`)
+console.log('2. Plug electric kettle and wait for it to boil')
+console.log(`3. Take coffee ${cup}`)
+console.log(`4. Instant ${coffee} powder into ${cup}`)
+console.log(`5. When water is boiled, unplug kettle and pour water into ${cup}`)
+console.log(`6. Add ${sugar}`)
+console.log(`6. Add ${milk}`)
+console.log("7. Mix and Drink!")
